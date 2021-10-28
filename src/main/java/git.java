dcf642b0b4a86f75327037f932d9eeb39b0b3300
hanhaichao <93064796+hanhaichao@users.noreply.github.com>
2021-10-28 23:48:48 +0800

    pull commit

--- src/main/java/git.java
+++ src/main/java/git.java
@@ -2,6 +2,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("dadadad,ka开始push了哈");
         System.out.println("git成功了dawdawdawdaw！！！");
-
+        System.out.println("pull成功了da未达到aw！！！");
     }
 }
