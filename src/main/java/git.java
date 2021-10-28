0aab97f8605c8c86546dcde9867335caef850313
chao <2201850471@qq.com>
2021-10-28 23:34:57 +0800

    Second3

--- src/main/java/git.java
+++ src/main/java/git.java
@@ -1,6 +1,7 @@
 public class git {
     public static void main(String[] args) {
-        System.out.println("dadadad");
+        System.out.println("dadadad,ka开始push了哈");
         System.out.println("git成功了dawdawdawdaw！！！");
+
     }
 }
